4e3e3f51e53e661131193e780ec60bd81b86e02c
unknown <simobotero7445@gmail.com>
2023-08-26 18:33:47 -0500

    color y about

--- movie/views.py
+++ movie/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     return render(request, 'home.html',{'name':'Simón Botero'})
+
+def about(request):
+    return render(request, 'about.html')
